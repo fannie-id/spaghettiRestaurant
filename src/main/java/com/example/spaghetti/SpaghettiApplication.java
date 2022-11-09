a82553f6f79e5d76b1e80c0f62efc339209fac63
fangfangwang <fannie-id@hotmail.com>
2022-11-09 11:12:56 +0100

    initial project

---
+++ src/main/java/com/example/spaghetti/SpaghettiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spaghetti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaghettiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaghettiApplication.class, args);
+    }
+
+}
